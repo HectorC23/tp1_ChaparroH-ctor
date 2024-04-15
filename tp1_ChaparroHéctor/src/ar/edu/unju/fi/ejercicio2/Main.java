3a376747ab2cf00e992ae7ca1b6bac338ed3ca67
Héctor Chaparro <hectorychaparro23@gmail.com>
2024-04-15 00:16:58 -0300

    Ejercicio 2

---
+++ tp1_ChaparroHéctor/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,42 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+//		El nombre de un país.
+		String pais = "Argentina"; 
+		
+//		La edad de una persona.
+		int edad = 23;
+		
+//		La altura de un edificio.
+		float altura = 1.64f;
+		
+//		El precio de un producto de supermercado.
+		double precio = 230.99;
+		
+//		Número de teléfono.
+		String telefono = "+54 9 388 4856948";
+		
+//		El cálculo de coseno de 0.5
+		double cos = 0.1736;
+		
+		System.out.println(pais);
+		System.out.println(edad);
+		System.out.println(altura);
+		System.out.println(precio);
+		System.out.println(telefono);
+		System.out.println(cos);
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
